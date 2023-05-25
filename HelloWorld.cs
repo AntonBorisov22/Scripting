20ccb36dafd5fd185c1f14eb5121edbb01299115
AntonBorisov22 <134637718+AntonBorisov22@users.noreply.github.com>
2023-05-25 20:20:40 +0300

    HelloWorld.cs

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -7,13 +7,13 @@ public class HelloWorld : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //Вывод в консоль приветствия миру
+        //Г‚Г»ГўГ®Г¤ Гў ГЄГ®Г­Г±Г®Г«Гј ГЇГ°ГЁГўГҐГІГ±ГІГўГЁГї Г¬ГЁГ°Гі
         print("Hello World");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        print("crickets")
     }
 }
